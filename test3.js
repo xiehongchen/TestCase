f93cec4fbb908b17d67a92468d09dbf06eda3e39
xiehongchen <1754581057@qq.com>
2023-11-16 18:00:30 +0800

    调整目录和文件

--- test3.js
+++
@@ -1,5 +0,0 @@
-// console.log(typeof null); // object
-// console.log(typeof typeof null);  // string
-// console.log(typeof typeof typeof null);  // string
-// console.log(typeof console.log(1)); // 1 undefined
-
