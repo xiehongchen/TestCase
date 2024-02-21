01fe07b45a9da3ac34128235eb160b9ae8b73ad6
xiehongchen <1754581057@qq.com>
2024-02-21 11:35:39 +0800

    笔记

--- test.js
+++ test.js
@@ -2,22 +2,37 @@
  * @Author: xiehongchen 1754581057@qq.com
  * @Date: 2023-11-17 11:08:53
  * @LastEditors: xiehongchen 1754581057@qq.com
- * @LastEditTime: 2024-02-20 16:30:50
+ * @LastEditTime: 2024-02-21 11:33:05
  * @FilePath: /TestCase/test.js
  * @Description: 
  * 认真学习每一天
  */
+/*
+ * @lc app=leetcode.cn id=80 lang=javascript
+ *
+ * [80] 删除有序数组中的重复项 II
+ */
 
-const promises = [
-    Promise.resolve('resolved'),
-    Promise.reject('rejected'),
-    Promise.resolve('resolved')
-  ];
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    const n = nums.length;
+    if (n <= 2) {
+        return n;
+    }
+    let slow = 2, fast = 2;
+    while (fast < n) {
+        if (nums[slow - 2] != nums[fast]) {
+            nums[slow] = nums[fast];
+            ++slow;
+        }
+        ++fast;
+    }
+    return slow;
+  };
+  // @lc code=end
   
-Promise.allSettled(promises)
-.then(results => {
-    console.log('results', results);
-})
-.catch(error => {
-    console.error(error);
-});
+  
