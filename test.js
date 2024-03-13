92c8d90091c8a1a4442040a5eb2f5cd26ae6cd20
xiehongchen <1754581057@qq.com>
2024-03-13 18:59:56 +0800

    fix

---
+++ test.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: xiehongchen 1754581057@qq.com
+ * @Date: 2023-11-17 11:08:53
+ * @LastEditors: xiehongchen 1754581057@qq.com
+ * @LastEditTime: 2024-03-04 10:53:23
+ * @FilePath: /TestCase/test.js
+ * @Description: 
+ * 认真学习每一天
+ */
+console.log(["1","2","3"].map((n) => parseInt(n)))
+
+console.log(parseInt('2'))
