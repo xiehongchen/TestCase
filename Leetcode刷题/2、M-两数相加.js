e9c387976c93510dc1f4f3e658183c41057520b2
xiehongchen <1754581057@qq.com>
2024-03-27 14:16:12 +0800

    feat: algorithm

--- Leetcode刷题/2、M-两数相加.js
+++ Leetcode刷题/2、M-两数相加.js
@@ -55,3 +55,62 @@ var addTwoNumbers = function (l1, l2) {
 
     return head;
 };
+/*
+ * @lc app=leetcode.cn id=2 lang=javascript
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+const l1 = new ListNode(2)
+l1.next = new ListNode(4)
+l1.next.next = new ListNode(3)
+const l2 = new ListNode(5)
+l2.next = new ListNode(6)
+l2.next.next = new ListNode(4)
+var addTwoNumbers = function (l1, l2) {
+  let n = 0
+  let head = null
+  let tail = null
+  while (l1 || l2) {
+    const n1 = l1 ? l1.val : 0
+    const n2 = l2 ? l2.val : 0
+    const sum = n1 + n2 + n
+    if (!head) {
+      head = tail = new ListNode(sum % 10)
+    } else {
+      tail.next = new ListNode(sum % 10)
+      tail = tail.next
+    }
+    n = Math.floor(sum / 10)
+    if (l1) {
+      l1 = l1.next
+    }
+    if (l2) {
+      l2 = l2.next
+    }
+  }
+  if (n > 0) {
+    tail.next = new ListNode(n)
+  }
+  return head
+};
+// @lc code=end
+
+console.log(addTwoNumbers(l1, l2))
