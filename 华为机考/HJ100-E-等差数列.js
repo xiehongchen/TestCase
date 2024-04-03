c469c741194a7d1f6742d7c94fa28d56632dbccd
xiehongchen <1754581057@qq.com>
2024-04-03 16:18:46 +0800

    24-4-3

--- 华为机考/HJ100-E-等差数列.js
+++ 华为机考/HJ100-E-等差数列.js
@@ -1,17 +1,16 @@
 const rl = require("readline").createInterface({ input: process.stdin });
 var iter = rl[Symbol.asyncIterator]();
 const readline = async () => (await iter.next()).value;
- 
+// 输入：275
+// 输出：113575
+// 说明：2+5+...+821+824=113575  
 void async function () {
     // Write your code here
     while(line = await readline()){
-        let arr = line.split('')
-        let temp = []
-        for (let i = 0; i < arr.length; i++) {
-            if (!temp.includes(arr[i])) {
-                temp.push(arr[i])
-            }
+        let n = 2
+        for (let i = 1; i < line; i++) {
+            n = 3 * i + 2 + n
         }
-        console.log(temp.length)
+        console.log(n)
     }
 }()
