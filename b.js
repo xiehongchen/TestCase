a37f00509bcb47f50052ed3ed58fbc52b0429518
xiehongchen <1754581057@qq.com>
2024-04-10 18:25:20 +0800

    2024-04-10

--- b.js
+++ b.js
@@ -1,25 +1,48 @@
 const rl = require("readline").createInterface({ input: process.stdin });
 var iter = rl[Symbol.asyncIterator]();
 const readline = async () => (await iter.next()).value;
-// A = B + 2C
-// 4
-// 2 7 3 0
+
 void async function () {
     // Write your code here
-    const arr = []
     while ((line = await readline())) {
-        arr.push(line)
-    }
-    const n = Number(arr[0])
-    const list = arr[1].split(' ').map(item => Number(item))
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (i == j) continue
-            const add = list[i] + list[j] * 2
-            console.log('add', add, list[i], list[j])
-            if (list.includes(add) && add !== list[i]) {
-                console.log('输出结果', list[i], list[j], add)
+        const chars = line.split('');
+        let maxDepth = 0;
+        const stack = [];
+        const map = new Map([
+            [')', '('],
+            [']', '['],
+            ['}', '{']
+        ]);
+    
+        for (const cur of chars) {
+            // 如果有左边括号的，直接加到stack数组里，然后判断最大值
+            if (cur === '(' || cur === '[' || cur === '{') {
+                stack.push(cur);
+                maxDepth = Math.max(maxDepth, stack.length);
             }
+            // 如果有左边括号的
+            if (cur === ')' || cur === ']' || cur === '}') {
+                // 当前stack数组为空，那说明这个是无效字符串，输出0
+                if (stack.length === 0) {
+                    console.log(0);
+                    return;
+                }
+                // 获取最后一位，因为未按正确顺序闭合的字符串
+                const left = stack[stack.length - 1];
+                const c = map.get(cur);
+                if (left === c) {
+                    stack.pop();
+                } else {
+                    console.log(0);
+                    return;
+                }
+            }
+        }
+    
+        if (stack.length === 0) {
+            console.log(maxDepth);
+        } else {
+            console.log(0)
         }
     }
 }()
