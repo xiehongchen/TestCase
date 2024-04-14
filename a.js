723047cc4144889d59eb422c961ea8c273278d7e
xiehongchen <1754581057@qq.com>
2024-04-14 16:53:28 +0800

    2024-04-14

--- a.js
+++ a.js
@@ -1,10 +1,37 @@
-var name = 'jack'
+const readline = require('readline');
 
-function show () {
-  console.log(name)
-  if(0) {
-    var name = 'tom'
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (line) => {
+  const n = parseInt(line.trim());
+  rl.once('line', (nums) => {
+    const arr = nums.trim().split(" ").map(num => parseInt(num));
+    const result = solution(arr);
+    process.stdout.write(result);
+  });
+});
+
+function solution(arr) {
+  if (arr.length === 0 || arr.length === 1 || (arr.length === 2 && arr[0] !== arr[1])) {
+    return "NO";
+  }
+  let min = arr[0];
+  let sum = min;
+  let temp = min;
+  for (let i = 1; i < arr.length; i++) {
+    sum += arr[i];
+    min = Math.min(min, arr[i]);
+    temp ^= arr[i];     console.log('sum',sum);
+  console.log('min', min);
+  console.log('temp', temp); 
+  }
+
+  if (temp !== 0) {
+    return "NO";
+  } else {
+    return String(sum - min);
   }
 }
-
-show()
--- b.js
+++ b.js
@@ -4,45 +4,27 @@ const readline = async () => (await iter.next()).value;
 
 void async function () {
     // Write your code here
+    const token = []
     while ((line = await readline())) {
-        const chars = line.split('');
-        let maxDepth = 0;
-        const stack = [];
-        const map = new Map([
-            [')', '('],
-            [']', '['],
-            ['}', '{']
-        ]);
-    
-        for (const cur of chars) {
-            // 如果有左边括号的，直接加到stack数组里，然后判断最大值
-            if (cur === '(' || cur === '[' || cur === '{') {
-                stack.push(cur);
-                maxDepth = Math.max(maxDepth, stack.length);
-            }
-            // 如果有左边括号的
-            if (cur === ')' || cur === ']' || cur === '}') {
-                // 当前stack数组为空，那说明这个是无效字符串，输出0
-                if (stack.length === 0) {
-                    console.log(0);
-                    return;
-                }
-                // 获取最后一位，因为未按正确顺序闭合的字符串
-                const left = stack[stack.length - 1];
-                const c = map.get(cur);
-                if (left === c) {
-                    stack.pop();
-                } else {
-                    console.log(0);
-                    return;
-                }
-            }
-        }
-    
-        if (stack.length === 0) {
-            console.log(maxDepth);
-        } else {
-            console.log(0)
-        }
+        token.push(line)
+    }
+    let n = token[0]
+    let arr = token[1].split(' ').map(Number)
+    if (arr.length === 0 || arr.length === 1 || (arr.length === 2 && arr[0] !== arr[1])) {
+        console.log("NO1");
+        return
+    }
+    let min = arr[0];
+    let sum = min;
+    let temp = min
+    for (let i = 1; i < arr.length; i++) {
+        sum += arr[i];
+        min = Math.min(min, arr[i]);
+        temp ^= arr[i];
+    }
+    if (temp !== 0) {
+        console.log("NO2");
+    } else {
+        console.log(String(sum - min));
     }
 }()
